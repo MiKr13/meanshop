83cfb00918fc6b0724a92bcdc9e6b0a40c1980aa
Stevens Garcia <smalmsteeng@gmail.com>
2018-01-02 17:10:23 -0500

    removed default generator module "thing"

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -13,20 +13,4 @@ describe('Controller: MainCtrl', function() {
   var $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function(_$httpBackend_, $controller, $rootScope, $state) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
-
-    scope = $rootScope.$new();
-    state = $state;
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should attach a list of things to the scope', function() {
-    $httpBackend.flush();
-    expect(scope.awesomeThings.length).to.equal(4);
-  });
 });
