7c6af587ab86e6dd3e844276b01152b651aac7c4
Stevens Garcia <smalmsteeng@gmail.com>
2018-01-24 13:27:35 -0500

    client:products : implemented catalog and search functionality

--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -3,10 +3,24 @@
 angular.module('meanshopApp')
   .factory('Products', ['$resource', function($resource) {
 
-    return $resource('api/products/:id', null,
+    return $resource('api/products/:id/:controller', null,
       {
         'update': {
           method: 'PUT'
+        },
+        'catalog': {
+          method: 'GET',
+          isArray: true,
+          params: {
+            controller: 'catalog'
+          }
+        },
+        'search': {
+          method: 'GET',
+          isArray: true,
+          params: {
+            controller: 'search'
+          }
         }
       });
 
