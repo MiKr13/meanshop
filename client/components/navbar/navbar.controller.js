cac255498f40884575e610d5f20d0c86534838c6
Stevens Garcia <smalmsteeng@gmail.com>
2018-01-24 13:14:02 -0500

    client:navbar : added search bar to navbar

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('meanshopApp')
-  .controller('NavbarCtrl', function ($scope, Auth) {
+  .controller('NavbarCtrl', function ($scope, Auth, $rootScope, $state, $window, $timeout) {
     $scope.menu = [{
       'title': 'Home',
       'state': 'main'
@@ -14,4 +14,28 @@ angular.module('meanshopApp')
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
+
+    /**
+     * Broadcast an event when typing anything in search bar
+     */
+    $scope.search = function() {
+      $rootScope.$broadcast('search:term', $scope.searchTerm);
+    }
+
+    /**
+     * Redirects to /products page as soon as user focuses on the
+     * search bar
+     */
+    $scope.redirect = function () {
+      $state.go('products');
+      // timeout makes sure that it is invoked after any other event has been triggered.
+      $timeout(function () {
+        // focus on search box
+        var searchBox = $window.document.getElementById('searchBox');
+        if(searchBox) {
+          searchBox.focus(); 
+        }
+      });
+    };
+
   });
